f8c5dd54f8a82eae52895bc5ad22957dd7dd3932
chudnofskyATD <mpereannor@gmail.com>
2019-10-03 07:28:11 +0000

    final touch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import useDarkMode from '../hooks/useDarkMode';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useDarkMode(false);
+  const [darkMode, setDarkMode] = useDarkMode('dark-mode', false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
